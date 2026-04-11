cf8c200aaa79cbf6f6b64fe3108c4fcbfa245a63
SpeedyTimer <jonathanrodriguez68t@gmail.com>
2026-04-11 12:12:53 -0600

    primer commit de agregando funcionalidades

--- src/main/java/com/trip/controllers/HomeController.java
+++ src/main/java/com/trip/controllers/HomeController.java
@@ -2,7 +2,9 @@ package com.trip.controllers;
 
 
 
+import java.util.List;
 import java.util.Date;
+import java.util.LinkedList;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +12,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+	
+	
+	
+	
+	
+	
 	@GetMapping("/")
 	public String mostrarhome(Model model) {
 		
@@ -25,6 +33,19 @@ public class HomeController {
 		
 		return "home";
 
+	}
+	@GetMapping("/listado")
+	public String mostrarListado(Model model) {
+		
+		List<String> lista= new LinkedList<String>();
+		lista.add("En la montana ");
+		lista.add("En la ciudad");
+		lista.add("En los pueblos");
+		lista.add("En las playas");
+		
+		model.addAttribute("listadoTrips", lista);
+		
+		return "listado";
 	}
 
 }
---
+++ src/main/java/com/trip/models/Categoria.java
@@ -0,0 +1,36 @@
+package com.trip.models;
+
+public class Categoria {
+	
+	private Integer id;
+	private String nomCategoria;
+	private String descripcion;
+	private boolean activo;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNomCategoria() {
+		return nomCategoria;
+	}
+	public void setNomCategoria(String nomCategoria) {
+		this.nomCategoria = nomCategoria;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public boolean isActivo() {
+		return activo;
+	}
+	public void setActivo(boolean activo) {
+		this.activo = activo;
+	}
+	
+
+
+}
