f8dc029878d5ffceec2f554acc484fd91c882f92
sNeroGG <bryanmn62@gmail.com>
2026-04-11 12:14:57 -0600

    Cambios 4/11 Diapo 17

--- src/main/java/com/trips/controllers/HomeController.java
+++ src/main/java/com/trips/controllers/HomeController.java
@@ -1,30 +1,45 @@
 package com.trips.controllers;
 
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 
 public class HomeController {
-	
+
 	@GetMapping("/")
-	public String mostrarHome (Model model) {
-		
+	public String mostrarHome(Model model) {
+
 		String trip = "Rapel en el Volcan";
 		Date fechaPublicacion = new Date();
 		double costo = 5.0;
-		boolean vigente= true;
+		boolean vigente = true;
 
-		model.addAttribute ("trip", trip); 
-		model.addAttribute("fechaPublicacion", fechaPublicacion); 
-		model.addAttribute ("costo", costo); 
-		model.addAttribute ("vigente", vigente);
-		
+		model.addAttribute("trip", trip);
+		model.addAttribute("fechaPublicacion", fechaPublicacion);
+		model.addAttribute("costo", costo);
+		model.addAttribute("vigente", vigente);
+
 		return "home";
+	}
+
+	@GetMapping("/")
+	public String MostrarListado(Model model) {
+		List<String> lista = new LinkedList<String>();
+		lista.add("En la montaña");
+		lista.add("En la Ciudad");
+		lista.add("En los pueblos");
+		lista.add("En las playas");
+
+		model.addAttribute("listadoTrips", lista);
+
+		return "listado";
 	}
 
 }
